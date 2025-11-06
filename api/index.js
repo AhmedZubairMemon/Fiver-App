8e2ccc1fde2400666781a6450709ef685586f994
Ahmed Zubair Memon <ahmedzubairmemon21@gmail.com>
2025-11-07 01:56:08 +0500

    Fiver App

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ dotenv.config()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:"http://localhost:5173", credentials:true}))
+app.use(cors({origin:"https://fiver-app-6xat.vercel.app", credentials:true}))
 
 const connect = async () =>{
     try{
