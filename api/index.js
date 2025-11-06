d76095371e3ca8c969c799471298c045a8bc65e3
Ahmed Zubair Memon <ahmedzubairmemon21@gmail.com>
2025-11-07 01:08:42 +0500

    Fiver App

--- api/index.js
+++ api/index.js
@@ -49,8 +49,7 @@ app.use((err, req, res, next)=>{
 if (process.env.NODE_ENV !== 'production') {
     const port = process.env.PORT || 8000;
     app.listen(port, () => {
+         connect()
         console.log(`Server is running on port ${port}`);
     });
 }
-
-export default app;
