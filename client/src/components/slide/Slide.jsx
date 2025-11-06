63bec25e0c9e62a5a4209e49aedec76297844efc
Ahmed Zubair Memon <ahmedzubairmemon21@gmail.com>
2025-11-07 01:31:31 +0500

    Fiver App

--- client/src/components/slide/Slide.jsx
+++ client/src/components/slide/Slide.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./Slide.scss";
-import Slider from "infinite-react-carousel";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Slide = ({ children, slidesToShow, arrowsScroll }) => {
+  const settings = {
+    slidesToShow: slidesToShow || 3,
+    slidesToScroll: arrowsScroll || 1,
+    arrows: true,
+    infinite: true,
+  };
+
   return (
     <div className="slide">
       <div className="container">
-        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
+        <Slider {...settings}>
           {children}
         </Slider>
       </div>
