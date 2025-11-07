e5a9b3b283f89d9505eb0808498aeaa7ebe350d8
Ahmed Zubair Memon <ahmedzubairmemon21@gmail.com>
2025-11-08 01:54:46 +0500

    Fiver App

--- api/controllers/order.controllers.js
+++ api/controllers/order.controllers.js
@@ -3,42 +3,42 @@ import createError from "../utils/createError.js"
 import Gig from "../models/gig.model.js"
 import Stripe from "stripe"
 
-export const intent = async (req, res, next) =>{
-    const stripe = new Stripe(
-        process.env.STRIPE
-    );
+export const intent = async (req, res, next) => {
+  try {
+    const stripe = new Stripe(process.env.STRIPE);
 
-    const gig = await Gig.findById(req.params.id)
+    const gig = await Gig.findById(req.params.id);
+    if (!gig) return next(createError(404, "Gig not found!"));
 
     const paymentIntent = await stripe.paymentIntents.create({
-  amount: gig.price * 100,
-  currency: 'usd',
-  automatic_payment_methods: {
-    enabled: true,
+      amount: gig.price * 100,
+      currency: "usd",
+      automatic_payment_methods: { enabled: true },
+    });
+
+    const newOrder = new Order({
+      gigId: gig._id,
+      img: gig.cover,
+      title: gig.title,
+      buyerId: req.userId,
+      sellerId: gig.userId,
+      price: gig.price,
+      payment_intent: paymentIntent.id,
+    });
+
+    await newOrder.save();
+
+    res.status(200).send({ clientSecret: paymentIntent.client_secret });
+  } catch (err) {
+    console.error("Stripe intent error:", err.message);
+    next(err);
   }
-});
-
-const newOrder = new Order({
-            gigId: gig._id,
-            img:gig.cover,
-            title:gig.title,
-            buyerId:req.userId,
-            sellerId:gig.userId,
-            price:gig.price,
-            payment_intent:paymentIntent.id,
-        })
-        
-        await newOrder.save()
-
-        res.status(200).send({
-            clientSecret: paymentIntent.client_secret
-        })
-}
+};
 
 export const getOrders = async(req, res, next)=>{
     try {
         const orders = await Order.find({
-            ...(req.isSaller ? {sellerId: req.userId} : {buyerId: req.userId}),
+            ...(req.isSeller ? {sellerId: req.userId} : {buyerId: req.userId}),
             isCompleted:true
     })
     res.status(200).send(orders);
